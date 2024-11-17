c401c198ea23e3d689582c0df5b55fde26de14a5
droooney <d.vor7@mail.ru>
2024-11-17 14:53:34 +0500

    improve types

--- lib/Provider.ts
+++ lib/Provider.ts
@@ -6,6 +6,8 @@ import { runHandlers } from './utils';
 export type ProviderContext<P> =
   P extends Provider<infer InputContext, infer ContextExtension> ? InputContext & ContextExtension : never;
 
+export type ProviderContextExtension<P> = P extends Provider<any, infer ContextExtension> ? ContextExtension : never;
+
 export abstract class Provider<InputContext extends object, ContextExtension extends object = {}> {
   private readonly _handlers: Handler<InputContext & ContextExtension>[];
 
